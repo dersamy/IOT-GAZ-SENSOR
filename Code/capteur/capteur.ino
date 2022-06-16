e6909a6c5588b27ace7c177ead671cdbc399a1fa
dersamy <derradji.r.samy@gmail.com>
2022-06-16 01:04:22 +0100

    code modified

--- Code/capteur/capteur.ino
+++ Code/capteur/capteur.ino
@@ -10,7 +10,7 @@ const char* password = "123456789";
 #define ADC A0
 #define Threshold 12
 
-unsigned long currentMillis(0),lastMillis(0),lastMillis1(0);
+unsigned long currentMillis(0), lastMillis(0), lastMillis1(0);
 float analogReading(0);
 int percent(0);
 bool threshold(0);
@@ -28,25 +28,28 @@ void setup() {
     ESP.restart();
   }
   ArduinoOTA.begin();
-  
+
   pinMode(GATE, OUTPUT);
   pinMode(ADC, INPUT);
   pinMode(Threshold, INPUT);
+
+  analogWrite(GATE, 71);
+
 }
 
 void loop() {
   ArduinoOTA.handle();
 
   currentMillis = millis();
-  
-  if (currentMillis - lastMillis >= 500){
+
+  if (currentMillis - lastMillis >= 1000 && isHIGH == false) {
     analogReading = analogRead(ADC);
     percent = analogReading * 0.09765625;
     threshold = digitalRead(Threshold);
 
-    ubidots.add("Gaz level", percent);// Change for your variable name  
+    ubidots.add("Gaz level", percent);// Change for your variable name
     ubidots.add("threshold", threshold);
-  
+
     bool bufferSent = false;
     bufferSent = ubidots.send();
 
@@ -56,18 +59,18 @@ void loop() {
     lastMillis = currentMillis;
   }
 
-  if(isHIGH == true){
-    if (currentMillis - lastMillis1 >= 60000){
+  if (isHIGH == true) {
+    if (currentMillis - lastMillis1 >= 60000) {
       analogWrite(GATE, 71);
       isHIGH = false;
-    lastMillis1 = currentMillis;
+      lastMillis1 = currentMillis;
     }
   }
-  else{
-    if (currentMillis - lastMillis1 >= 90000){
+  else {
+    if (currentMillis - lastMillis1 >= 90000) {
       analogWrite(GATE, 255);
       isHIGH = true;
-    lastMillis1 = currentMillis;
+      lastMillis1 = currentMillis;
     }
   }
 }
